b7c25e1d27c6213bb5fa5132af6fc071287702ce
ibharath462 <ibharath462@gmail.com>
2018-03-06 22:56:43 +0530

    first commit

---
+++ app/src/test/java/v1/f1nd/com/f1nd_newfeatures/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package v1.f1nd.com.f1nd_newfeatures;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
